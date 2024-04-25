9fbc925c02b947722ae87b28e365ffd99ad82331
YourSmallFriend <SamKint10@hotmail.com>
2024-04-25 13:29:54 +0200

    text size change

--- aanmelding-formulier-project/Form1.cs
+++ aanmelding-formulier-project/Form1.cs
@@ -15,11 +15,15 @@ namespace aanmelding_formulier_project
         public Form1()
         {
             InitializeComponent();
+            var standaardTXTsize = new System.Drawing.Font("Arial", 20);
+            voornaamTXT.Font = standaardTXTsize;
+            tussenvoegselTXT.Font = standaardTXTsize;
+            achternaamTXT.Font = standaardTXTsize;
+
         }
 
         private void aanmeldBTN_Click(object sender, EventArgs e)
         {
-            //test
             string filePath = "aanmeldingformulier.csv";
             string content = string.Format("\n{0},{1},{2}", voornaamTXT.Text, tussenvoegselTXT.Text, achternaamTXT.Text);
             try
@@ -33,7 +37,7 @@ namespace aanmelding_formulier_project
                     File.WriteAllText(filePath, content);
                 }
 
-                MessageBox.Show("CSV content saved successfully!");
+                MessageBox.Show("CSV content is opgeslagen!");
             }
             catch (Exception ex)
             {

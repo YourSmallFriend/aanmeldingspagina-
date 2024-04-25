60b4d9e4b94781849ce359b0f02d0ca768b86006
YourSmallFriend <SamKint10@hotmail.com>
2024-04-25 12:08:47 +0200

    commit 1

---
+++ aanmelding-formulier-project/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace aanmelding_formulier_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aanmeldBTN_Click(object sender, EventArgs e)
+        {
+            string filePath = "aanmeldingformulier.csv";
+            string content = string.Format("\n{0},{1},{2}", voornaamTXT.Text, tussenvoegselTXT.Text, achternaamTXT.Text);
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    File.AppendAllText(filePath, content);
+                }
+                else
+                {
+                    File.WriteAllText(filePath, content);
+                }
+
+                MessageBox.Show("CSV content saved successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while saving the CSV content: {ex.Message}");
+            }
+        }
+
+
+    }
+}
